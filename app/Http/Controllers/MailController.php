7b30e8960dc6200506bfc04b70fc1757e42f0fb3
Muhammed Fatih İNANÇ <mfatihinanc@gmail.com>
2019-02-05 21:48:38 +0000

    Activation content mail is changed.

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,7 +10,7 @@ class MailController extends Controller
 {
     public function SendActivationMail($mail, $activation_code)
     {
-        Mail::html('In order to activate your account, <a href="'.env('SERVICE_ADDRESS_USER').'/users/'.$activation_code.'/activate">please click the link.</a>', function($message) use($mail) {
+        Mail::html('In order to activate your account, <a href="'.env('ADDRESS_WEB').'/Home/Activation/'.$activation_code.'">please click the link.</a>', function($message) use($mail) {
             $message->to($mail)->subject('TrackKnowledge Activation Mail');
         });
         return response()->json('', 200);
